b7849c1bd8a93bd10a3d33ca02b7b435985f64a4
Andriy <sherudylo.andrii@lll.kpi.ua>
2026-01-05 16:31:02 +0200

    05.01.2026 (1)

--- Script.js
+++ Script.js
@@ -2,12 +2,29 @@ function SwapText() {
     const item2 = document.querySelector('.item2'); // Header
     const item6 = document.querySelector('.item6'); // Footer
 
-    if (item2 && item6) {
-        let temp = item2.innerHTML;
-        item2.innerHTML = item6.innerHTML;
-        item6.innerHTML = temp;
-        console.log("Тексти змінили місця.");
-    }
+    const temp = item2.innerHTML;
+    item2.innerHTML = item6.innerHTML;
+    item6.innerHTML = temp;
+
+    //console.log(item2);
+    //console.log(item6);
+} SwapText();
+
+function FindAreaOfDiamond() {
+    const d1 = 15;
+    const d2 = 10;
+    const S = (d1 * d2) / 2;
+
+    document.getElementById("Result").textContent = `Area Of Diamond: ${S}`;
+
+    //console.log(S);
+
+} FindAreaOfDiamond();
+
+
+document.getElementById("Button_AddNumberInArray").onclick() = function(){
+    console.log("Hi");
+
 }
-// Викликаємо функцію відразу при завантаженні
-SwapText();
+
+
